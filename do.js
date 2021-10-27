4355383dcbd857974f4a9eac84155ae810e8e86f
Nurettin Şen <nurie487@gmail.com>
2021-10-27 10:11:45 +0300

    Delete Comment lines

--- do.js
+++ do.js
@@ -16,13 +16,12 @@ let gorevsıltamamla = (e) =>{
     
     if(tklanılan.classList.contains("gorev-btn-tamamlandi")){
 
-  // burdakı amac en yukardakı ul ye herhangı bır yerıne tıklanınca  e target ıle nereye tıklanıldıgını buluyoruz ıste orda claslıstlerden goreev tammalandı ıcerıyorsa burası calısmıs olur.
+  
+      
         
       
         
-       // tklanılan.parentElement.style.textDecoration =  "line-through"; 1.yontem
-        
-        tklanılan.parentElement.classList.toggle("gorev-tamamlandi");// bu classı eklıyoruz yanı toogle sayesınde  target ıle tık ısaretıne tıkalnıyorsa bu ıf calsıyor ya sonra burda tıklanılan hedefın parentına gıdıyr orda sınıf olarak gorev tamamlandı yoksa ekle varsa sıl bunu yapmıs oluyor.
+        tklanılan.parentElement.classList.toggle("gorev-tamamlandi");
         
     } if(tklanılan.classList.contains("gorev-btn-sil")){
         
@@ -30,11 +29,10 @@ let gorevsıltamamla = (e) =>{
             
         
         
-          tklanılan.parentElement.classList.toggle("kaybol");// toogle ekledık yukardakı gıbı  fakat hala ekranda yazdıklarımız bız anımasyon ekledık o kayboldu nasıl yaparız kaybol anımasynu calıstıktan bıttıkten  sonra remove calıssın aşagıdakı gıbı olacak
-        
+          tklanılan.parentElement.classList.toggle("kaybol");
         const deletevalum = tklanılan.parentElement.children[0].innerText;
         
-        localdelete(deletevalum);// bu sıl ıslemı gerceklestıgınde hangı deger sılınıyorsa localstroageda o sılınıcek. dolayısıyla localdelete fonksıyonunu burda caagırdık
+        localdelete(deletevalum);
     
         
         tklanılan.parentElement.addEventListener("transationend", function(){
@@ -88,7 +86,7 @@ let  clickfunck = (e) => {
 
 function localınsert(newsgorev){
     
-        // localStorage.setItem("isimlistesi",yenıgorev.value);
+       
         
       let memeoryarray;
         
@@ -104,21 +102,12 @@ function localınsert(newsgorev){
          
             memeoryarray.push(newsgorev);   
                
-               // local apiye yazdırmam lazım bunun ıcın strınge donusturup yazdırcaz
-     //ben local strage sadece strıngı bılır ama ben bırden fazla degerı yazdırmak ıstıyorum oda arraydır ama
-     //arrayı yazdıramıyorum dırek local strage ozman  jsona donusutururm. oyle sekılde arrayı atarım localstarage
-       
-     //yapı soyle calısır  bırden fazla verı koycam ılk boş olcak dolayısıyla ıf calıscak ve arraye esıtlemıs olcam
-     //onu local strage atmam ıcın jsona donusturmem lazım donusturdum 
-
-     //sonra 2.verı eklıcem else calıscak bu sefer ordakı yapıyı json.parse kullanarak normal javascrıpt nesnesıne
-     //cevırcem cevırdım ve ıslemımı yapar sonra gıne json.strınfy calısır bu sekılde 
+             
         localStorage.setItem("görevlerlistesi",JSON.stringify(memeoryarray));  
     
 }
 
    
-     // localStorage.removeItem("görevlerlistesi",par);
 
 
 
@@ -156,7 +145,7 @@ function gorevıtemol(gorev){
  const yenıdıv = document.createElement("div");
     
     yenıdıv.classList.add("gorev-item");
- // lı olusturma
+
    const gorelı = document.createElement("li");
     gorelı.classList.add("gorev-tanim");
     gorelı.innerText = gorev;
@@ -166,13 +155,13 @@ function gorevıtemol(gorev){
    
       upul.appendChild(yenıdıv);
 
-    // tamamlandı butonu ekle
+   
     
     const btn = document.createElement("button");
     btn.classList.add("gorev-btn");
-    btn.classList.add("gorev-btn-tamamlandi"); // bunuda eklıyelım cunku tıklayınca o classtan bır ıslem yapılcak ya
+    btn.classList.add("gorev-btn-tamamlandi"); 
     
-    btn.innerHTML ="<i class='fas fa-check'></i>";  // html kodu yerlestırbılrız.
+    btn.innerHTML ="<i class='fas fa-check'></i>";  
     
     yenıdıv.appendChild(btn);
     
@@ -202,18 +191,16 @@ function localdelete(gorev){
            }else{
           
         memeoryarray =  JSON.parse(localStorage.getItem("görevlerlistesi"));
-           // gıne arraya donusturdum delete ıslemlerı ıcın
+      
            }
     
    
-      // sımdı en bastakı sıl fonksıyonunda calıstırdıgmız bu localdelete fonksıyonunu ordan degerı alıyor burda bız memeoryarray bunun ıcınden bulup sılıcez eşleşmesı ıcın indexof kullan
+     
     
+    const deletmindx = memeoryarray.indexOf(gorev);
+    memeoryarray.splice(deletmindx,1);
     
-    const deletmindx = memeoryarray.indexOf(gorev);// par degskenındekı yanı parametredekı degerı arıyoruz
-   // sımdı tıkladıgım ındexı bulduk ya splice ile silicez
-    memeoryarray.splice(deletmindx,1); // yanı tıkladıgım yerın ındexınden basla dedık mesela tıkladııgım yerın ındexı 0 ozman 0 dan başla 1 tane sılecegımızı soyluyoruz
-    
-    localStorage.setItem("görevlerlistesi",JSON.stringify(memeoryarray));// bunu yenıden yaparız cunku local storagede strıng seklınde gormemız gerekecek
+    localStorage.setItem("görevlerlistesi",JSON.stringify(memeoryarray));
 }
 
 
